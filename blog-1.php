d2aae73a700e55ac26e8890dc19ef8ab5f86030c
mugilavathi <you@example.com>
2025-04-17 16:54:58 +0530

    sitemap changed

--- blog-1.php
+++ blog-1.php
@@ -189,7 +189,7 @@
     <script src="vendor/noUiSlider/nouislider.min.js"></script>
 
     <!-- Main JS-->
-    <script src="js/global.min.js"></script>
+    <script src="js/global.js"></script>
 
 </body>
 
